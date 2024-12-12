2585db9f09079a73933a3cd3b535d337475b46f2
Andre Thomas <drethedevjs@gmail.com>
2024-12-12 14:18:05 -0500

    Abstracted out the mobile header logo.

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -7,10 +7,25 @@ import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import styles from './Header.module.css';
 
+const HeaderLogo = () => {
+  return (
+    <div className='w-full place-items-center'>
+      <Link href="/">
+        <Image
+          src="/images/logo/augusta-ga-wedding-photographer-logo-submrk.png"
+          width={300}
+          height={200}
+          alt='Covenant LX Logo'
+        />
+      </Link>
+    </div>
+  );
+}
+
 export default function Header() {
   const [ mobileMenuOpen, setMobileMenuOpen ] = useState<boolean>(false);
   const router = useRouter();
-  
+
   const navigate = (path: string) => {
     toggleMenu();
     router.push(path);
@@ -47,28 +62,14 @@ export default function Header() {
           <div className='place-content-center text-covGray ml-4'>
             <Bars2Icon className='size-10' onClick={toggleMenu} />
           </div>
-          <div className='w-full place-items-center'>
-            <Image
-              src="/images/logo/augusta-ga-wedding-photographer-logo-submrk.png"
-              width={300}
-              height={200}
-              alt='Covenant LX Logo'
-            />
-          </div>
+          <HeaderLogo />
         </div>
         <div className={`${styles["mobile-links"]} ${mobileMenuOpen ? 'flex flex-col' : 'hidden'}`}>
           <div className='p-5 flex flex-row mb-10 bg-white'>
             <div className='place-content-center text-covGray ml-4'>
               <XMarkIcon className='size-10' onClick={toggleMenu} />
             </div>
-            <div className='w-full place-items-center'>
-              <Image
-                src="/images/logo/augusta-ga-wedding-photographer-logo-submrk.png"
-                width={300}
-                height={200}
-                alt='Covenant LX Logo'
-              />
-            </div>
+            <HeaderLogo />
           </div>
           <ul className='py-10 text-3xl'>
             <li onClick={() => navigate("/")} className={styles.li}>Home</li>
