322636dfe2efea73506eb95d017b47331b5bb84d
Andre Thomas <drethedevjs@gmail.com>
2024-12-12 16:49:50 -0500

    Linked the quick links.

--- src/app/components/Quick Links/QuickLinks.tsx
+++ src/app/components/Quick Links/QuickLinks.tsx
@@ -1,19 +1,31 @@
-import styles from './QuickLinks.module.css'
+import Link from 'next/link';
+import styles from './QuickLinks.module.css';
+
 export default function QuickLinks() {
   return (
     <div className="grid md:grid-cols-3 md:gap-0 gap-8 my-16 text-covGray">
-      <div className={styles.block}>
-        <h3 className={styles.h3}>Information</h3>
-        <p className={styles.p}>Investment & Details</p>
-      </div>
+      <Link href="/investment">
+        <div className={styles.block}>
+          <h3 className={styles.h3}>Information</h3>
+          <p className={styles.p}>Investment & Details</p>
+        </div>
+      </Link>
       <div className={`${styles.block} ${styles.middle}`}>
         <h3 className={styles.h3}>Portfolio</h3>
         <p className={styles.p}>see my work!</p>
       </div>
-      <div className={styles.block}>
-        <h3 className={styles.h3}>Stay in Touch</h3>
-        <p className={styles.p}>FB and Pin</p>
-      </div>
+        <div className={styles.block}>
+          <h3 className={`${styles.h3} mb-0`}>Stay in Touch</h3>
+          <p className={styles.p}>
+            <Link href="https://facebook.com/covenantlx" target='_blank' className='hover:text-gold hover:font-semibold'>
+              Facebook 
+            </Link>
+            &nbsp;//&nbsp;
+            <Link href="https://www.pinterest.com/covenantlx/" target='_blank' className='hover:text-gold hover:font-semibold'>
+              Pinterest
+            </Link>
+          </p>
+        </div>
     </div>
   )
 }
