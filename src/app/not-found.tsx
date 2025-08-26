87b71c594fed3670a4b244f7dce40eb8547025b1
Andre Thomas <drethedevjs@gmail.com>
2025-08-26 10:22:59 -0400

    Fixed typo on not found page.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div className="mx-30 text-center">
       <h1 className="text-7xl my-10">Not Found.</h1>
       <p className="mb-10">
-        Uh oh! Looks like you gotten lost. No worries. Get back on track by
+        Uh oh! Looks like you've gotten lost. No worries. Get back on track by
         visiting one of the galleries below.
       </p>
       <div className="container mx-auto lg:px-32 px-10 text-center">
