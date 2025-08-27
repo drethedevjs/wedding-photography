bf12e3eaed1c4398e5691d48e7a5e4ad285bbda5
Andre Thomas <drethedevjs@gmail.com>
2025-08-26 20:52:27 -0400

    Create client side popular gallery component.

---
+++ src/app/components/PopularGalleriesClient/PopularGalleriesClient.tsx
@@ -0,0 +1,52 @@
+"use client";
+import imageHelper from "@/utils/ImageHelper";
+import { _Object } from "@aws-sdk/client-s3";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import styles from "./PopularGalleriesClient.module.css";
+
+export default function PopularGalleries() {
+  const [homeImageData, setHomeImageData] = useState<_Object[]>();
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const res = await fetch("/api/images?prefix=home");
+      const data: _Object[] = await res.json();
+      setHomeImageData(data);
+    };
+    fetchImages();
+  }, []);
+  if (!homeImageData) return;
+
+  return (
+    <section>
+      <h3 className={`${styles.h3}`}>Popular Galleries</h3>
+      <small className={`${styles.small}`}>Click to view a gallery</small>
+      <div className={`${styles.galleries}`}>
+        <Image
+          src={imageHelper.getImageSrc(homeImageData!, "muff")}
+          width={300}
+          height={450}
+          alt="Two people looking at each other"
+          className={`${styles.image}`}
+        />
+
+        <Image
+          src={imageHelper.getImageSrc(homeImageData!, "shaffer")}
+          width={300}
+          height={450}
+          alt="Two people looking at each other"
+          className={`${styles.image}`}
+        />
+
+        <Image
+          src={imageHelper.getImageSrc(homeImageData!, "anderson")}
+          width={300}
+          height={450}
+          alt="Two people looking at each other"
+          className={`${styles.image}`}
+        />
+      </div>
+    </section>
+  );
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,13 @@
-'use client'
-import { CoupleFormData } from '@/interface/CoupleFormData';
-import { yupResolver } from '@hookform/resolvers/yup';
-import axios from 'axios';
+"use client";
+import { CoupleFormData } from "@/interface/CoupleFormData";
+import { yupResolver } from "@hookform/resolvers/yup";
+import axios from "axios";
 import "dotenv/config";
-import { useState } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { object, string } from 'yup';
-import PopularGalleries from '../components/PopularGalleries/PopularGalleries';
-import styles from './Contact.module.css';
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { object, string } from "yup";
+import PopularGalleriesClient from "../components/PopularGalleriesClient/PopularGalleriesClient";
+import styles from "./Contact.module.css";
 
 const schema = object({
   brideName: string().required("Bride Name is required"),
@@ -16,7 +16,9 @@ const schema = object({
   weddingDate: string().required("Wedding Date is required"),
   phone: string().required("Phone is required"),
   venue: string().required("Venue is required"),
-  message: string().min(15, "Message isn't long enough").required("Message is required"),
+  message: string()
+    .min(15, "Message isn't long enough")
+    .required("Message is required")
 });
 
 export default function Contact() {
@@ -26,24 +28,28 @@ export default function Contact() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<CoupleFormData>({ resolver: yupResolver<CoupleFormData>(schema) })
+    formState: { errors }
+  } = useForm<CoupleFormData>({
+    resolver: yupResolver<CoupleFormData>(schema)
+  });
 
-  const handleFormSubmit: SubmitHandler<CoupleFormData> = async (formData) => {
+  const handleFormSubmit: SubmitHandler<CoupleFormData> = async formData => {
     setLoading(true);
 
     try {
-      await axios.post(`${process.env.NEXT_PUBLIC_BASE_MAILER_URL}/api/email/send-covenant`, formData);
+      await axios.post(
+        `${process.env.NEXT_PUBLIC_BASE_MAILER_URL}/api/email/send-covenant`,
+        formData
+      );
       setHasSent(true);
     } catch (err: unknown) {
-      if (axios.isAxiosError(err))
-        console.error(err.message);
+      if (axios.isAxiosError(err)) console.error(err.message);
 
-      console.log("An unexpected error has occurred:", err)
+      console.error("An unexpected error has occurred:", err);
     }
 
     setLoading(false);
-  }
+  };
 
   return (
     <>
@@ -51,53 +57,92 @@ export default function Contact() {
         <h1 className="pageH1">Contact</h1>
         <div className={hasSent ? "hidden" : ""}>
           <form onSubmit={handleSubmit(handleFormSubmit)}>
-            <div className='grid md:grid-cols-2 grid-cols-1 gap-5 mb-10'>
+            <div className="grid md:grid-cols-2 grid-cols-1 gap-5 mb-10">
               <div>
-                <label htmlFor='brideName'>Bride Name</label>
-                <input type='text' id='brideName' placeholder='First and Last' {...register("brideName")} />
-                <small className='text-red-500'>{errors.brideName?.message}</small>
+                <label htmlFor="brideName">Bride Name</label>
+                <input
+                  type="text"
+                  id="brideName"
+                  placeholder="First and Last"
+                  {...register("brideName")}
+                />
+                <small className="text-red-500">
+                  {errors.brideName?.message}
+                </small>
               </div>
               <div>
-                <label htmlFor='groomName'>Groom Name</label>
-                <input type='text' id='groomName' placeholder='First and Last' {...register("groomName")} />
-                <small className='text-red-500'>{errors.groomName?.message}</small>
+                <label htmlFor="groomName">Groom Name</label>
+                <input
+                  type="text"
+                  id="groomName"
+                  placeholder="First and Last"
+                  {...register("groomName")}
+                />
+                <small className="text-red-500">
+                  {errors.groomName?.message}
+                </small>
               </div>
               <div>
-                <label htmlFor='email'>Email</label>
-                <input type='email' id='email' {...register("email")} />
-                <small className='text-red-500'>{errors.email?.message}</small>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" {...register("email")} />
+                <small className="text-red-500">{errors.email?.message}</small>
               </div>
               <div>
-                <label htmlFor='weddingDate'>Wedding Date</label>
-                <input type='date' id='weddingDate' {...register("weddingDate")} />
-                <small className='text-red-500'>{errors.weddingDate?.message}</small>
+                <label htmlFor="weddingDate">Wedding Date</label>
+                <input
+                  type="date"
+                  id="weddingDate"
+                  {...register("weddingDate")}
+                />
+                <small className="text-red-500">
+                  {errors.weddingDate?.message}
+                </small>
               </div>
               <div>
-                <label htmlFor='phone'>Phone</label>
-                <input type='tel' id='phone' placeholder='555-555-5555' {...register("phone")} />
-                <small className='text-red-500'>{errors.phone?.message}</small>
+                <label htmlFor="phone">Phone</label>
+                <input
+                  type="tel"
+                  id="phone"
+                  placeholder="555-555-5555"
+                  {...register("phone")}
+                />
+                <small className="text-red-500">{errors.phone?.message}</small>
               </div>
               <div>
-                <label htmlFor='venue'>Venue</label>
-                <input type='text' id='venue' {...register("venue")} />
-                <small className='text-red-500'>{errors.venue?.message}</small>
+                <label htmlFor="venue">Venue</label>
+                <input type="text" id="venue" {...register("venue")} />
+                <small className="text-red-500">{errors.venue?.message}</small>
               </div>
-              <div className='md:col-span-2'>
-                <label htmlFor='message'>Message</label>
-                <textarea id='message' placeholder='Tell us about you and your wedding. How can I help?' rows={6} {...register("message")} />
-                <small className='text-red-500'>{errors.message?.message}</small>
+              <div className="md:col-span-2">
+                <label htmlFor="message">Message</label>
+                <textarea
+                  id="message"
+                  placeholder="Tell us about you and your wedding. How can I help?"
+                  rows={6}
+                  {...register("message")}
+                />
+                <small className="text-red-500">
+                  {errors.message?.message}
+                </small>
               </div>
             </div>
-            <button className='disabled:opacity-30 disabled:cursor-progress' disabled={loading}>Send</button>
+            <button
+              className="disabled:opacity-30 disabled:cursor-progress"
+              disabled={loading}
+            >
+              Send
+            </button>
           </form>
         </div>
         <div className={`${styles.sentMsg} ${hasSent ? "" : "hidden"}`}>
           <h2 className={`${styles.h2}`}>Sent</h2>
-          <p className={`${styles.p}`}>Thank you for your message. I will be in touch very soon!</p>
+          <p className={`${styles.p}`}>
+            Thank you for your message. I will be in touch very soon!
+          </p>
 
-          <PopularGalleries />
+          <PopularGalleriesClient />
         </div>
       </div>
     </>
-  )
+  );
 }
