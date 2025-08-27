e28afdd30964e5b1c97d20122033a3f82d4b2860
Andre Thomas <drethedevjs@gmail.com>
2025-08-26 22:48:26 -0400

    Updated loading template page.

--- src/app/gallery/loading.tsx
+++ src/app/gallery/loading.tsx
@@ -1,9 +1,9 @@
 export default function Loading() {
   return (
-    <div>
-      <p className="text-6xl text-center mt-16 text-skyBlue animate-bounce">
-        Loading...
+    <section className="place-content-center grid min-h-screen">
+      <p className="text-6xl text-skyBlue animate-bounce">
+        Loading gallery. Thank you for your patience.
       </p>
-    </div>
+    </section>
   );
 }
