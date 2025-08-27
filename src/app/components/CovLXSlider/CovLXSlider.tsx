e00fc0c0b6054a6ff2a283760be1f7eed02f064c
Andre Thomas <drethedevjs@gmail.com>
2025-08-27 17:55:33 -0400

    Added a mobile view slider.

--- src/app/components/CovLXSlider/CovLXSlider.tsx
+++ src/app/components/CovLXSlider/CovLXSlider.tsx
@@ -2,7 +2,7 @@
 import imageHelper from "@/utils/ImageHelper";
 import { _Object } from "@aws-sdk/client-s3";
 import Image from "next/image";
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "./CovLXSlider.module.css";
 
@@ -12,35 +12,68 @@ export default function CovLXSlider({
   galleryImageData: _Object[];
 }) {
   return (
-    <Swiper
-      slidesPerView={"auto"}
-      centeredSlides={true}
-      spaceBetween={30}
-      pagination={{
-        clickable: true
-      }}
-      loop={true}
-      navigation={true}
-      modules={[Navigation]}
-      className="mt-10"
-    >
-      {galleryImageData.map((image, index) => {
-        return (
-          <SwiperSlide key={image.Key} className={styles.slide}>
-            <div className={styles.slideWrapper}>
-              <Image
-                src={imageHelper.getImageSrc(galleryImageData, image.Key!)}
-                alt={`Augusta, GA Wedding Photographer - ${index + 1}`}
-                width={800}
-                height={1200}
-                priority={index === 0}
-                className={styles.image}
-                style={{ objectFit: "contain" }}
-              />
-            </div>
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+    <>
+      {/* Desktop slider */}
+      <Swiper
+        slidesPerView={"auto"}
+        centeredSlides={true}
+        spaceBetween={30}
+        pagination={{
+          clickable: true
+        }}
+        loop={true}
+        navigation={true}
+        modules={[Navigation, Pagination]}
+        className="mt-10 md:flex hidden"
+      >
+        {galleryImageData.map((image, index) => {
+          return (
+            <SwiperSlide key={image.Key} className={styles.slide}>
+              <div className={styles.slideWrapper}>
+                <Image
+                  src={imageHelper.getImageSrc(galleryImageData, image.Key!)}
+                  alt={`Augusta, GA Wedding Photographer - ${index + 1}`}
+                  width={800}
+                  height={1200}
+                  priority={index === 0}
+                  className={styles.image}
+                  style={{ objectFit: "contain" }}
+                />
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+
+      {/* Mobile Slider */}
+      <Swiper
+        direction={"vertical"}
+        slidesPerView={"auto"}
+        spaceBetween={0}
+        freeMode={true}
+        pagination={{
+          clickable: true
+        }}
+        modules={[Pagination]}
+        className={`mt-10 flex md:hidden ${styles.mobileSwiper}`}
+      >
+        {galleryImageData.map((image, index) => {
+          return (
+            <SwiperSlide key={image.Key} className={styles.mobileSlide}>
+              <div className={styles.mobileImageWrapper}>
+                <Image
+                  src={imageHelper.getImageSrc(galleryImageData, image.Key!)}
+                  alt={`Augusta, GA Wedding Photographer - ${index + 1}`}
+                  width={400}
+                  height={600}
+                  priority={index === 0}
+                  className={styles.mobileImage}
+                />
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
   );
 }
