6a8e8b3c2076322882b8924585f0daa4f44cc8a5
Andre Thomas <drethedevjs@gmail.com>
2025-08-26 20:51:15 -0400

    Made about and investment page SSR.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import Link from "next/link";
 import HeroInsetText from "../components/HeroInsetText/HeroInsetText";
 import TextTitleBlock from "../components/TextTitleBlock/TextTitleBlock";
@@ -11,34 +9,44 @@ export default function About() {
       <HeroInsetText />
       <div className="container mx-auto lg:px-32 md:px-20 px-10 mt-10">
         <p className="text-center mb-10">
-          Covenant LX exists to serve engaged couples in the CSRA by providing 
-          them with bright and colorful lifestyle portraits. I'm Andre Thomas and 
-          I am thrilled that I get to connect with couples and to capture an array 
-          of portraits that will allow them to relive a very special day. It is nice 
-          to meet you. I look forward to connecting with you to discuss your wedding day, 
-          to get to know you, and to share with you all that Covenant LX has to offer.
+          Covenant LX exists to serve engaged couples in the CSRA by providing
+          them with bright and colorful lifestyle portraits. I'm Andre Thomas
+          and I am thrilled that I get to connect with couples and to capture an
+          array of portraits that will allow them to relive a very special day.
+          It is nice to meet you. I look forward to connecting with you to
+          discuss your wedding day, to get to know you, and to share with you
+          all that Covenant LX has to offer.
         </p>
         <hr className="border-t-4 border-gold mb-5" />
-        <h2 className="text-center uppercase text-3xl tracking tracking-widest text-gold">Augusta Wedding Photographer</h2>
+        <h2 className="text-center uppercase text-3xl tracking tracking-widest text-gold">
+          Augusta Wedding Photographer
+        </h2>
         <hr className="border-t-2 border-b border-gold mt-5" />
 
         <div className="mt-10">
           <TextTitleBlock heading="Faith">
-            If there's anything about me that I want to shine through the most, 
-            it's that I am a man of faith (Christian). I am thankful that the Lord 
-            graciously choose to reveal Himself to me and saved me during my college 
-            years. This fact defines me and everything that I do.
+            If there's anything about me that I want to shine through the most,
+            it's that I am a man of faith (Christian). I am thankful that the
+            Lord graciously choose to reveal Himself to me and saved me during
+            my college years. This fact defines me and everything that I do.
           </TextTitleBlock>
         </div>
         <TextTitleBlockMulti heading="Family">
           <>
-            Another thing that I want people to know about me is that I love my family: my amazing wife and my two wonderful kids. When I die, I want it said of me that, “if there was one thing that Andre was good at was being a great husband and a loving father.”
+            Another thing that I want people to know about me is that I love my
+            family: my amazing wife and my two wonderful kids. When I die, I
+            want it said of me that, “if there was one thing that Andre was good
+            at was being a great husband and a loving father.”
           </>
           <>
-            It's a beautiful thing when a man commit himself to the woman he loves. Not to romanticize the union–because marriage is hard–but the best things in life aren’t easy.
+            It's a beautiful thing when a man commit himself to the woman he
+            loves. Not to romanticize the union–because marriage is hard–but the
+            best things in life aren’t easy.
           </>
           <>
-            Weddings are the establishment of families and families are the cornerstone of our society. This is why weddings are meaningful to me.
+            Weddings are the establishment of families and families are the
+            cornerstone of our society. This is why weddings are meaningful to
+            me.
           </>
         </TextTitleBlockMulti>
 
@@ -47,5 +55,5 @@ export default function About() {
         </Link>
       </div>
     </>
-  )
+  );
 }
--- src/app/investment/page.tsx
+++ src/app/investment/page.tsx
@@ -1,10 +1,17 @@
-"use client"
-
+import imageHelper from "@/utils/ImageHelper";
+import { _Object } from "@aws-sdk/client-s3";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./Investment.module.css";
 
-export default function Investment() {
+export default async function Investment() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/images?prefix=investment`
+  );
+
+  const imageData: _Object[] = await res.json();
+  if (!imageData) return;
+
   return (
     <>
       <div className="container mx-auto lg:px-32 px-10 text-center">
@@ -12,26 +19,32 @@ export default function Investment() {
         <div className="flex xl:flex-row flex-col">
           <div className="hidden xl:flex">
             <Image
-              src={`${process.env.NEXT_PUBLIC_CDN_PREFIX}/investment/augusta-ga-wedding-photographer-coco.jpg`}
+              src={imageHelper.getImageSrc(imageData!, "coco")}
               width={1080}
               height={1920}
               alt="A bride standing on the beach facing the ocean and looking back at the camera."
+              priority
             />
           </div>
           <div className="xl:px-16 px-10 place-content-center w-full">
             <p className={`${styles.p} mb-5`}>
-              Thank you for considering us for your wedding photography and/or videography needs. We're 
-              looking forward to the opportunity to work with you two.
+              Thank you for considering us for your wedding photography and/or
+              videography needs. We're looking forward to the opportunity to
+              work with you two.
             </p>
             <p className={`${styles.p} mb-5`}>
-              My pricing starts out at $1599 but most of my couples spend in the ballpark of $2200-2300. 
-              I don't want to waste your time with pricing sheets and collections. I used to do that but 
-              I have found that couples can get stuck paying for products and services that they don't 
-              need in order to get extra time or an extra photographer. How about this: Let's sit down and 
-              chat about what your needs and values are and I can build a collection specifically for you?
+              My pricing starts out at $1599 but most of my couples spend in the
+              ballpark of $2200-2300. I don't want to waste your time with
+              pricing sheets and collections. I used to do that but I have found
+              that couples can get stuck paying for products and services that
+              they don't need in order to get extra time or an extra
+              photographer. How about this: Let's sit down and chat about what
+              your needs and values are and I can build a collection
+              specifically for you?
             </p>
             <p className={`${styles.p}`}>
-              I'm looking forward to you becoming a part of our gallery of brides.
+              I'm looking forward to you becoming a part of our gallery of
+              brides.
             </p>
 
             <Link href="/contact">
@@ -41,5 +54,5 @@ export default function Investment() {
         </div>
       </div>
     </>
-  )
+  );
 }
