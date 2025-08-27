c895fcbc07ee7f11af2451306ecc883beee81a0b
Andre Thomas <drethedevjs@gmail.com>
2025-08-26 20:39:14 -0400

    Made the Gallery component SS and created a loading component for it.

---
+++ src/app/gallery/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div>
+      <p className="text-6xl text-center mt-16 text-skyBlue animate-bounce">
+        Loading...
+      </p>
+    </div>
+  );
+}
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,12 +1,10 @@
-// "use client"
-import { Suspense } from 'react';
-import GalleryComponent from '../components/Gallery/Gallery';
+import GalleryComponent from "../components/Gallery/Gallery";
 
-export default async function Gallery({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) {
+export default async function Gallery({
+  searchParams
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
   const galleryId = Number((await searchParams).id);
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <GalleryComponent galleryId={galleryId} />
-    </Suspense>
-  );
+  return <GalleryComponent galleryId={galleryId} />;
 }
