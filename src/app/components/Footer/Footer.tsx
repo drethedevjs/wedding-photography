3d80f69fa33e64dafcccedae7e41e5d9f335f6b6
Andre Thomas <drethedevjs@gmail.com>
2025-08-28 05:38:56 -0400

    Fixed footer spacing.

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,8 +1,6 @@
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer>
-      Copyright Covenant LX Wedding Photography 2016 - {currentYear}
-    </footer>
+    <footer>© Covenant LX Wedding Photography 2016 - {currentYear}</footer>
   );
 }
