ff3d1a840e2beff04519c6c023a3e410a9511f51
Andre Thomas <drethedevjs@gmail.com>
2025-08-29 07:26:18 -0400

    Removed unused property from gallery.

--- src/data/galleries.ts
+++ src/data/galleries.ts
@@ -5,7 +5,6 @@ const galleries: Gallery[] = [
     id: 1,
     name: "Pritchard Wedding",
     heroImageTag: "pritchard",
-    heroImageUrl: "/images/gallery/Wedding-Photography-Pritchard-FeatImg.jpg",
     heroImageAlt:
       "Bride and groom in a close embrace, bride adjusting groom's bow tie.",
     isActive: true
@@ -14,7 +13,6 @@ const galleries: Gallery[] = [
     id: 2,
     name: "Martin Wedding",
     heroImageTag: "martin",
-    heroImageUrl: "/images/home/augusta-ga-wedding-photographer-2.jpg",
     heroImageAlt:
       "Bride and bridesmaids locking arms and looking at one another..",
     isActive: true
@@ -23,7 +21,6 @@ const galleries: Gallery[] = [
     id: 3,
     name: "Muff Wedding",
     heroImageTag: "muff",
-    heroImageUrl: "/images/gallery/Wedding-Photography-Muff-FeatImg.jpg",
     heroImageAlt: "Bridal party posing like they're on a hip hop album cover.",
     isActive: true
   },
@@ -31,7 +28,6 @@ const galleries: Gallery[] = [
     id: 4,
     name: "Shaffer Engagement",
     heroImageTag: "shaffer",
-    heroImageUrl: "/images/gallery/Wedding-Photography-Shaffer-FeatImg.jpg",
     heroImageAlt:
       "An engaged couple standing at the corner of a barn looking separate ways.",
     isActive: true
@@ -40,7 +36,6 @@ const galleries: Gallery[] = [
     id: 5,
     name: "Anderson Wedding",
     heroImageTag: "anderson",
-    heroImageUrl: "/images/gallery/Wedding-photography-anderson-FeatImg.jpg",
     heroImageAlt:
       "Bride and groom standing in classic prom pose, bride laughing.",
     isActive: true
@@ -49,7 +44,6 @@ const galleries: Gallery[] = [
     id: 6,
     name: "Jewell Engagement",
     heroImageTag: "jewell",
-    heroImageUrl: "/images/gallery/Wedding-photographer-Jewell-FeatImg.jpg",
     heroImageAlt:
       "Bride and groom in a close embrace, bride adjusting groom's bow tie.",
     isActive: true
@@ -58,7 +52,6 @@ const galleries: Gallery[] = [
     id: 7,
     name: "Hacker Wedding",
     heroImageTag: "hacker",
-    heroImageUrl: "/images/gallery/hacker/Wedding-Photography-Hacker-435.jpg",
     heroImageAlt:
       "Bride and groom in a close embrace, bride adjusting groom's bow tie.",
     isActive: true
@@ -67,8 +60,6 @@ const galleries: Gallery[] = [
     id: 8,
     name: "Matarelli Wedding",
     heroImageTag: "matarelli",
-    heroImageUrl:
-      "/images/gallery/matarelli/Wedding-Photography-Matarelli-340.jpg",
     heroImageAlt:
       "Bride and bridesmaids locking arms and looking at one another.",
     isActive: false
@@ -77,8 +68,6 @@ const galleries: Gallery[] = [
     id: 9,
     name: "Hargraves Wedding",
     heroImageTag: "hargraves",
-    heroImageUrl:
-      "/images/gallery/hargraves/Wedding-Photographer-Hargraves-23.jpg",
     heroImageAlt:
       "Bride and bridesmaids locking arms and looking at one another.",
     isActive: true
--- src/interface/Gallery.ts
+++ src/interface/Gallery.ts
@@ -2,7 +2,6 @@ export interface Gallery {
   id: number; // Unique identifier for the gallery
   name: string; // Name of the gallery
   heroImageTag: string;
-  heroImageUrl: string; // URL of the hero image
   heroImageAlt: string; // Alt text for the hero image
   isActive: boolean;
 }
